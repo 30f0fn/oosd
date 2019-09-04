64397eac5bdbee87dab5461563ec779073c5d98c
Max Weiss <30f0fn@gmail.com>
2019-09-04 11:40:23 -0400

    add directories for cs681, and reconfigure build scripts

---
+++ src/edu/umb/cs681/hw00/HomeworkRunner.java
@@ -0,0 +1,28 @@
+package edu.umb.cs681.hw00;
+
+// import edu.umb.cs680.hw01.Calculator;
+// import edu.umb.cs680.hw02.Student;
+// import edu.umb.cs680.hw03.Singleton;
+// import edu.umb.cs680.hw04.Car;
+
+class HomeworkRunner {
+    
+    public static void main(String[] args) {
+        System.out.printf("Running homework exercises for cs681...\n");
+        // Calculator.main(args);
+        // Student.main(args);
+        // Singleton.main(args);
+        // Car.main(args);
+        // edu.umb.cs680.hw05.FileSystem.main(args);
+        // edu.umb.cs680.hw06.FileSystem.main(args);
+        // edu.umb.cs680.hw07.FileSystem.main(args);
+        // edu.umb.cs680.hw08.FileSystem.main(args);
+        // edu.umb.cs680.hw09.CarViewer.main(args);
+        // edu.umb.cs680.hw10.StockQuoteObservable.main(args);
+        // edu.umb.cs680.hw11.Glass.main(args);
+        // edu.umb.cs680.hw12.APFS.main(args);
+        // edu.umb.cs680.hw13.DVDPlayer.main(args);
+        // edu.umb.cs680.hw14.CarViewer.main(args);
+        // edu.umb.cs680.hw15.APFS.main(args);
+    }
+}

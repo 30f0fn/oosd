9d0f65a1f8663f8b3e928212dd564bcbc5a367e5
Max Weiss <30f0fn@gmail.com>
2019-09-05 20:40:29 -0400

    add use of generic observer from cs 680 hw 11

--- src/edu/umb/cs681/hw01/Main.java
+++ src/edu/umb/cs681/hw01/Main.java
@@ -1,18 +1,24 @@
 package edu.umb.cs681.hw01;
 
-import edu.umb.cs680.hw10.MyObservable;
 import edu.umb.cs680.hw10.MyObserver;
 import edu.umb.cs680.hw10.DJIAEvent;
 import edu.umb.cs680.hw10.DJIAQuoteObserver;
 import edu.umb.cs680.hw10.StockEvent;
 import edu.umb.cs680.hw10.StockQuoteObserver;
 
+import edu.umb.cs680.hw11.ObservableEnhanced;
+import edu.umb.cs680.hw11.ObserverEnhanced;
 
 public class Main {
     public static void main(String[] args) {
+
+        //////////////////////////////////////////////////////////
+        // cs 680 homework 10 - observer pattern implementation //
+        //////////////////////////////////////////////////////////
+
         StockQuoteObservable sqObservable = new StockQuoteObservable();
         MyObserver threeDeeStockQuoteRenderer = (Object e) ->
-            System.out.printf("Wow a Three Dee rendering of stock %s as %f!\n",
+            System.out.printf("Wow a Three Dee rendering of stock %s at %f!\n",
                               ((StockEvent) e).getTicker(),
                               ((StockEvent) e).getQuote());
         sqObservable.addObserver((MyObserver) threeDeeStockQuoteRenderer);
@@ -20,11 +26,26 @@ public class Main {
 
         DJIAQuoteObservable djiaObservable = new DJIAQuoteObservable();
         MyObserver threeDeeDJIARenderer = (Object e) -> {
-            System.out.printf("Wow a Three Dee rendering of DJIA as %f!\n",
+            System.out.printf("Wow a Three Dee rendering of DJIA at %f!\n",
                               ((DJIAEvent) e).getQuote());
         };
         djiaObservable.addObserver((MyObserver) threeDeeDJIARenderer);
         djiaObservable.setQuote(9999f);
+
+        
+        ///////////////////////////////////////////
+        // cs 680 homework 11 - generic observer //
+        ///////////////////////////////////////////
+        StockQuoteObservableEnhanced sqObservableE =
+            new StockQuoteObservableEnhanced();
+        ObserverEnhanced<StockEvent> fourDeeStockQuoteRendererE =
+            (StockEvent e) ->
+            System.out.printf("Wow a Four Dee rendering of stock %s at %f!\n",
+                              ((StockEvent) e).getTicker(),
+                              ((StockEvent) e).getQuote());
+        sqObservableE.addObserver(fourDeeStockQuoteRendererE);
+        sqObservableE.changeQuote("ENRN", 9f);
+
     }
     
 }

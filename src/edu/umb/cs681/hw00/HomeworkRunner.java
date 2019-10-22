69061cd0bd14ef10f60ef63ddb63f884dd60220a
Max Weiss <30f0fn@gmail.com>
2019-10-22 14:51:23 -0400

    clean up 682 HomeworkRunner

--- src/edu/umb/cs681/hw00/HomeworkRunner.java
+++ src/edu/umb/cs681/hw00/HomeworkRunner.java
@@ -1,10 +1,5 @@
 package edu.umb.cs681.hw00;
 
-// import edu.umb.cs680.hw01.Calculator;
-// import edu.umb.cs680.hw02.Student;
-// import edu.umb.cs680.hw03.Singleton;
-// import edu.umb.cs680.hw04.Car;
-
 class HomeworkRunner {
     
     public static void main(String[] args) {
@@ -13,20 +8,11 @@ class HomeworkRunner {
         edu.umb.cs681.hw02.Main.main(args);
         edu.umb.cs681.hw03.Main.main(args);
         edu.umb.cs681.hw04.Main.main(args);
-        // Calculator.main(args);
-        // Student.main(args);
-        // Singleton.main(args);
-        // Car.main(args);
-        // edu.umb.cs680.hw05.FileSystem.main(args);
-        // edu.umb.cs680.hw06.FileSystem.main(args);
-        // edu.umb.cs680.hw07.FileSystem.main(args);
-        // edu.umb.cs680.hw08.FileSystem.main(args);
-        // edu.umb.cs680.hw09.CarViewer.main(args);
-        // edu.umb.cs680.hw10.StockQuoteObservable.main(args);
-        // edu.umb.cs680.hw11.Glass.main(args);
-        // edu.umb.cs680.hw12.APFS.main(args);
-        // edu.umb.cs680.hw13.DVDPlayer.main(args);
-        // edu.umb.cs680.hw14.CarViewer.main(args);
-        // edu.umb.cs680.hw15.APFS.main(args);
+        edu.umb.cs681.hw05.Main.main(args);
+        edu.umb.cs681.hw06.Main.main(args);
+        // edu.umb.cs681.hw07.Main.main(args);
+        // edu.umb.cs681.hw08.Main.main(args);
+        // edu.umb.cs681.hw09.Main.main(args);
+        // edu.umb.cs681.hw10.Main.main(args);
     }
 }

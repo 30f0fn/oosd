ecdfe58dbbe9a6139cb074b6f509b338b53fc975
Max Weiss <30f0fn@gmail.com>
2019-10-23 14:29:50 -0400

    hw 09

---
+++ src/edu/umb/cs681/hw09/Main.java
@@ -0,0 +1,21 @@
+package edu.umb.cs681.hw09;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HW08...\n\tRunning RunnableInterruptiblePrimeGenerator for 1 ms...");
+        RunnableInterruptiblePrimeGenerator gen =
+            new RunnableInterruptiblePrimeGenerator(1, 10000000L);
+        try {
+            Thread thread = new Thread(gen);
+            thread.start();
+            Thread.sleep(1);
+            thread.interrupt();
+        thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        gen.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
+        System.out.println("\n\t" + gen.getPrimes().size() + " prime numbers are found.");
+    }
+}
---
+++ src/edu/umb/cs681/hw09/RunnableInterruptiblePrimeGenerator.java
@@ -0,0 +1,33 @@
+package edu.umb.cs681.hw09;
+
+import java.util.concurrent.locks.ReentrantLock;
+import edu.umb.cs681.threads.primes.RunnablePrimeGenerator;
+
+
+public class RunnableInterruptiblePrimeGenerator extends RunnablePrimeGenerator {
+    private boolean done = false;
+    private ReentrantLock lock = new ReentrantLock();
+	
+    public RunnableInterruptiblePrimeGenerator(long from, long to) {
+        super(from, to);
+    }
+
+    public void generatePrimes(){
+        for (long n = from; n <= to; n++) {
+            lock.lock();
+            try {
+                if (Thread.interrupted()) {
+                    // System.out.println("Stopped generating prime numbers.");
+                    // this.primes.clear();
+                    break;
+                }
+                if (isPrime(n)) {
+                    this.primes.add(n);
+                }
+            } finally {
+                lock.unlock();
+            };
+        }
+    }
+
+}
